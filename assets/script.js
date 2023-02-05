137883ac8e2207d77a59907106575933d75c789a
Jordan <jordanfurneri57@gmail.com>
2023-02-05 10:32:37 +0100

    Ajout const arrow + event listener sur chaque fleches avec un console.log

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,33 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+// Constante Arrow
+const leftArrow = document.querySelector("#prev");
+const rightArrow = document.querySelector("#next");
+
+leftArrow.addEventListener("click", () => {
+  //Console log pour voir si la flêches fonctionne
+  console.log("Arrow Left fonctionne");
+});
+
+rightArrow.addEventListener("click", () => {
+  //Console log pour voir si la flêches fonctionne
+  console.log("Arrow Right fonctionne");
+});
