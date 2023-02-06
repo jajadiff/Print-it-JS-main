88f0b066cd66e7494ee2b513c7a3e003b9b5fdca
Jordan <jordanfurneri57@gmail.com>
2023-02-06 14:13:14 +0100

    Ajout fonction , updateDots , nextArrow , prevArrow

--- assets/script.js
+++ assets/script.js
@@ -18,16 +18,50 @@ const slides = [
   },
 ];
 
+let actualSlide = 0;
 // Constante Arrow
 const leftArrow = document.querySelector("#prev");
 const rightArrow = document.querySelector("#next");
 
 leftArrow.addEventListener("click", () => {
-  //Console log pour voir si la flêches fonctionne
+  //Console log pour voir si la flêches répond au click
   console.log("Arrow Left fonctionne");
+  prevArrow();
+  updateDots();
 });
 
 rightArrow.addEventListener("click", () => {
-  //Console log pour voir si la flêches fonctionne
+  //Console log pour voir si la flêches répond au click
   console.log("Arrow Right fonctionne");
+  nextArrow();
+  updateDots();
 });
+
+function prevArrow() {
+  if (actualSlide > 0) {
+    actualSlide = actualSlide - 1;
+  } else {
+    actualSlide = slides.length - 1;
+  }
+  console.log("prev");
+}
+
+function nextArrow() {
+  if (actualSlide < slides.length - 1) {
+    actualSlide = actualSlide + 1;
+  } else {
+    actualSlide = 0;
+  }
+  console.log("next");
+}
+
+function updateDots() {
+  const dots = document.querySelectorAll(".dot");
+  for (let i = 0; i < dots.length; i++) {
+    if (i === actualSlide) {
+      dots[i].classList.add("dot_selected");
+    } else {
+      dots[i].classList.remove("dot_selected");
+    }
+  }
+}
