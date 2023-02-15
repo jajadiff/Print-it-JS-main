2e4b480b38b7ffc3151e970edab9f5e3e646ab28
Jordan <jordanfurneri57@gmail.com>
2023-02-15 06:16:04 +0100

    Ajout de commentaire de le fichier script.js

--- assets/script.js
+++ assets/script.js
@@ -17,12 +17,14 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
-
+// Variable slides actual
 let actualSlide = 0;
-// Constante Arrow
+// Constante qui sélectionne class arrow left et right
 const leftArrow = document.querySelector("#prev");
 const rightArrow = document.querySelector("#next");
 
+// Ajout des event Listeners sur les flêches avec un evenement Click et
+// une function anonyme flêcher "Lamdba"
 leftArrow.addEventListener("click", () => {
   //Console log pour voir si la flêches répond au click
   console.log("Arrow Left fonctionne");
@@ -41,35 +43,54 @@ rightArrow.addEventListener("click", () => {
   updateTexte();
 });
 
+// Création function prevArrow (correspond a left arrow)
 function prevArrow() {
+  // Si avec la condition actualSlides est supérieur a 0
   if (actualSlide > 0) {
+    // Décrémentation de 1
     actualSlide = actualSlide - 1;
   } else {
+    // Sinon décrémentation de - 1 sur le tableau slides
     actualSlide = slides.length - 1;
   }
   console.log("prev");
 }
 
+// Création function nextArrow (correspond a right arrow)
 function nextArrow() {
+  // Si la condition actualSlides est inférieur à la longueur du tableau slides - 1 = true
   if (actualSlide < slides.length - 1) {
+    // Alors actualSlides + 1
     actualSlide = actualSlide + 1;
   } else {
+    // Sinon actualSlides = 0 il retourne a l'index 0 du tableau
     actualSlide = 0;
   }
   console.log("next");
 }
 
+// Création function updateDots
 function updateDots() {
+  // Création d'une constante dots avec cette fois querySelectorAll pour toute les class dot
+  // querySelectorAll récupere tous les éléments HTML (tableau)
   const dots = document.querySelectorAll(".dot");
+  // 1er valeur let i = 0 correspond au compteur pour le nombre de la boucle
+  // 2eme condition)  i est inférieur à dots.length
+  // 3eme condition) i + 1
   for (let i = 0; i < dots.length; i++) {
+    // Si i est strictement égalité à actualSlides
     if (i === actualSlide) {
+      // dots[i] = syntaxe pour accéder à l'élément du tableau dots
+      // classlist.add ajoute une class dot_selected
       dots[i].classList.add("dot_selected");
     } else {
+      // classlist.remove supprime la class dot_selected
       dots[i].classList.remove("dot_selected");
     }
   }
 }
 
+// Création function Images
 function updateImages() {
   const pictures = document.querySelector(".banner-img");
   const actualPictures = slides[actualSlide];
@@ -77,6 +98,7 @@ function updateImages() {
   pictures.alt = actualPictures.tagLine;
 }
 
+// Création function Textes
 function updateTexte() {
   const textes = document.querySelector("#banner-txt");
   const actualTexte = slides[actualSlide];
